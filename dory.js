6080971622ed896abec16816477fa7920a92a3e8
André Neves <ami.neves@campus.fct.unl.pt>
2016-12-08 15:19:02 +0000

    Add files via upload

--- dory.js
+++ dory.js
@@ -1,12 +1,27 @@
 window.onload = function init() {
     
-   myAudio = new Audio('dory.ogg'); 
-	  myAudio.addEventListener('ended', function() {
-    	this.currentTime = 0;
-   	 this.play();
-	}, false);
-	myAudio.play();
-	for(;;)
-	document.write("I SUFFER FROM SHORT TERM MEMORY LOSS!!!! ");
   
+        myAudio5 = new Audio('dory.ogg'); 
+        myAudio5.addEventListener('ended', function() {
+    	this.currentTime = 0;
+   	    this.play();
+	}, false);
+	myAudio5.play();
+   
+	 myAudio4 = new Audio('enva.mp3'); 
+        myAudio4.addEventListener('ended', function() {
+    	this.currentTime = 0;
+   	    this.play();
+            this.volume = 100;
+	}, false);
+	myAudio4.play();
+
+  render();
+}
+
+function render() 
+{
+   
+    document.write("I SUFFER FROM SHORT TERM MEMORY LOSS!!!! ");
+    window.requestAnimationFrame(render);
 }
