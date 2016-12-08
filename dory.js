e592c0764bd1bb702557308dd68db47a37fb5e4a
André Neves <ami.neves@campus.fct.unl.pt>
2016-12-08 15:07:17 +0000

    Update dory.js

--- dory.js
+++ dory.js
@@ -6,59 +6,7 @@ window.onload = function init() {
    	 this.play();
 	}, false);
 	myAudio.play();
-	  myAudio = new Audio('dory.ogg'); 
-	  myAudio.addEventListener('ended', function() {
-    	this.currentTime = 0;
-   	 this.play();
-	}, false);
-	myAudio.play();
-	  myAudio = new Audio('dory.ogg'); 
-	  myAudio.addEventListener('ended', function() {
-    	this.currentTime = 0;
-   	 this.play();
-	}, false);
-	myAudio.play();
-	  myAudio = new Audio('dory.ogg'); 
-	  myAudio.addEventListener('ended', function() {
-    	this.currentTime = 0;
-   	 this.play();
-	}, false);
-	myAudio.play();
-	  myAudio = new Audio('dory.ogg'); 
-	  myAudio.addEventListener('ended', function() {
-    	this.currentTime = 0;
-   	 this.play();
-	}, false);
-	myAudio.play();
-	  myAudio = new Audio('dory.ogg'); 
-	  myAudio.addEventListener('ended', function() {
-    	this.currentTime = 0;
-   	 this.play();
-	}, false);
-	myAudio.play();
-	  myAudio = new Audio('dory.ogg'); 
-	  myAudio.addEventListener('ended', function() {
-    	this.currentTime = 0;
-   	 this.play();
-	}, false);
-	myAudio.play();
-	  myAudio = new Audio('dory.ogg'); 
-	  myAudio.addEventListener('ended', function() {
-    	this.currentTime = 0;
-   	 this.play();
-	}, false);
-	myAudio.play();
-	  myAudio = new Audio('dory.ogg'); 
-	  myAudio.addEventListener('ended', function() {
-    	this.currentTime = 0;
-   	 this.play();
-	}, false);
-	myAudio.play();
-	  myAudio = new Audio('dory.ogg'); 
-	  myAudio.addEventListener('ended', function() {
-    	this.currentTime = 0;
-   	 this.play();
-	}, false);
-	myAudio.play();
+	for(;;)
+	document.write("I SUFFER FROM SHORT TERM MEMORY LOSS!!!! ");
   
 }
