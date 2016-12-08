79f3fc303a501ef95b7f6b20cdbe5d2b7c38bd4b
André Neves <ami.neves@campus.fct.unl.pt>
2016-12-08 15:02:25 +0000

    Update dory.js

--- dory.js
+++ dory.js
@@ -1,1 +1,10 @@
-
+window.onload = function init() {
+    
+   myAudio = new Audio('dory.ogg'); 
+	  myAudio.addEventListener('ended', function() {
+    	this.currentTime = 0;
+   	 this.play();
+	}, false);
+	myAudio.play();
+  
+}
